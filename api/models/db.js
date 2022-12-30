6b83d11dcc111df70523690c1803793102ca96ce
mesto1830 <97528383+mesto1830@users.noreply.github.com>
2022-12-30 18:24:54 -0500

    Update db.js

--- api/models/db.js
+++ api/models/db.js
@@ -6,22 +6,4 @@ const db = mongoose.connect(process.env.mongo_url,{
   console.log('Connected')
 }).catch((err)=>{console.log(err)})
 
-// mongoose.connection.on('connected', function(){
-//   console.log(connected("Mongoose default connection is open to ", process.env.DATA));
-// });
-
-// mongoose.connection.on('error', function(err){
-//   console.log(error("Mongoose default connection has occured "+err+" error"));
-// });
-
-// mongoose.connection.on('disconnected', function(){
-//   console.log(disconnected("Mongoose default connection is disconnected"));
-// });
-
-// process.on('SIGINT', function(){
-//   mongoose.connection.close(function(){
-//       console.log(termination("Mongoose default connection is disconnected due to application termination"));
-//       process.exit(0)
-//   });
-// });
 module.exports = db
